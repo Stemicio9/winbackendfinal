8c6c507b3ee4a4202c2ebc8f8bd575f142266e32
Stemicio9 <s.miceli90@gmail.com>
2021-07-29 17:53:59 +0200

    Initial commit

---
+++ src/main/java/com/winbackendfinal/winbackendfinal/WinbackendfinalApplication.java
@@ -0,0 +1,13 @@
+package com.winbackendfinal.winbackendfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinbackendfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinbackendfinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winbackendfinal/winbackendfinal/WinbackendfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winbackendfinal.winbackendfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinbackendfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
